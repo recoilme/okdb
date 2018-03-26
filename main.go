d70c259c1f8c638334a03cd8e92aad9ef9b78f9c
Vadim Kilibaba <vkulibaba@surfy.ru>
2018-03-26 12:21:56 +0300

    params

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -18,8 +19,12 @@ import (
 // main start a gRPC server and waits for connection
 // run from root: protoc -I api/ -I${GOPATH}/src --go_out=plugins=grpc:api api/api.proto
 func main() {
+
+	grpcPort := flag.Int("grpc", 7777, "grpc port number, default 7777")
+	httpAddr := flag.String("http", ":5000", "http api address, default :5000")
+	flag.Parse()
 	// create a listener on TCP port 7777
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 7777))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *grpcPort))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -39,7 +44,7 @@ func main() {
 	}()
 
 	// start http server
-	Serve(":5000")
+	Serve(*httpAddr)
 
 	// handle kill
 	signalChan := make(chan os.Signal, 1)
